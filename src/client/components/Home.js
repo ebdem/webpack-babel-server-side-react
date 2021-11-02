96d9ac927f255a9aaaa4dceda32dc75934804005
ebdem <64805742+ebdem@users.noreply.github.com>
2021-11-02 15:34:56 +0300

    first config

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <div>I am the Ebubekir Naber home component</div>
+            <button onClick={() => console.log("Clicked")}>Click Me</button>
+        </div>
+    );
+};
+
+export default Home;
